2c1f94e269b60649ec2441d218d8df0a84dc157b
Syakhisk Al-Azmi <mail.syakhisk@gmail.com>
2023-03-11 17:33:24 +0700

    update

--- has-internet.py
+++ has-internet.py
@@ -3,20 +3,23 @@
 import os
 import requests
 import subprocess
+from datetime import datetime
 
 requests.packages.urllib3.disable_warnings()
 
 res = requests.get("https://google.com", verify=False)
 last_url = res.history[-1].url
 
+if "https://google.com" in last_url:
+    exit(0)
+
+print(f"[{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}]")
+print("Requesting internet access...")
+
 for h in res.history:
     print(h.url)
 
-if "https://google.com" in last_url:
-    print("Has Internet Access!")
-    exit(0)
-
 login_script = f"{os.getcwd()}/main.py"
 p = subprocess.Popen(login_script, stdout=subprocess.PIPE, shell=True)
-stdout,_ = p.communicate()
-print(stdout.decode())
+# stdout,_ = p.communicate()
+# print(stdout.decode())
