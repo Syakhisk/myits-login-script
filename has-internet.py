7cd16a043bbfe9aa9544d2eb8813cd0d5f9c977a
Syakhisk Al-Azmi <mail.syakhisk@gmail.com>
2023-03-14 21:47:13 +0700

    update

--- has-internet.py
+++ has-internet.py
@@ -7,10 +7,12 @@ from datetime import datetime
 
 requests.packages.urllib3.disable_warnings()
 
+script_path=os.path.dirname(os.path.realpath(__file__))
+
 res = requests.get("https://google.com", verify=False)
 last_url = res.history[-1].url
 
-if "https://myits-app.its.ac.id/internet" not in last_url:
+if last_url.startswith("https://google.com"):
     exit(0)
 
 print(f"[{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}]")
@@ -19,7 +21,7 @@ print("Requesting internet access...")
 for h in res.history:
     print(h.url)
 
-login_script = f"{os.getcwd()}/main.py"
+login_script = f"cd {script_path} && ./main.py"
 p = subprocess.Popen(login_script, stdout=subprocess.PIPE, shell=True)
 # stdout,_ = p.communicate()
 # print(stdout.decode())
