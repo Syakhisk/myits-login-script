7737df41e728bb40e5dff6a0eb452e9fc275012c
Syakhisk Al-Azmi <mail.syakhisk@gmail.com>
2023-08-05 00:55:06 +0700

    update notify

--- notify-uptime.sh
+++ notify-uptime.sh
@@ -2,4 +2,5 @@
 
 # Notify uptime using uptime-kuma
 
-curl -v "https://uptime-pve.up.railway.app/api/push/GDfazHsDQJ?status=up&msg=OK&ping="
+IP="$(hostname --ip-address)"
+curl -v "https://uptime-pve.up.railway.app/api/push/ESB1uvlcmN?status=up&msg=$IP&ping="
